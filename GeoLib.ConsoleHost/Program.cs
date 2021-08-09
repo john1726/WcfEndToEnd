153f834fa0c8d84adc7b2909b73faf7a05195b76
Roger Moore <roger@sound-qi.com>
2021-08-09 15:02:54 -0600

    -Added service hosting project Geolib.ConsoleHost

---
+++ GeoLib.ConsoleHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using GeoLib.Contracts;
+using GeoLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+
+            // Add endpoint configuration details procedurally here or in app.config
+            //string address = "net.tcp://localhost:8009/GeoService";
+            //Binding binding = new NetTcpBinding();
+            //Type contract = typeof(IGeoService);
+
+            //hostGeoManager.AddServiceEndpoint(contract, binding, address);
+
+            hostGeoManager.Open();
+
+            Console.WriteLine("Services started. Press [Enter] to exit.");
+            Console.ReadLine();
+
+            hostGeoManager.Close();
+        }
+    }
+}
